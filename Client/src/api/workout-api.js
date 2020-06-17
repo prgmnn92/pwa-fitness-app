19a74d6ce2824179526e9f696f57b8d4e791cbfb
Phillip Pargmann <pargmann92@gmail.com>
2020-06-17 16:03:05 +0200

    added momentjs and completed weekview

---
+++ Client/src/api/workout-api.js
@@ -0,0 +1,18 @@
+export const postWorkout = (workoutData, title, time) => {
+  let data = {
+    title: title,
+    duration: time,
+    exercises: workoutData,
+  };
+
+  fetch("http://localhost:5000/workout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.text())
+    .then((data) => console.log("success", data))
+    .catch((error) => console.log("error message: ", error));
+};
